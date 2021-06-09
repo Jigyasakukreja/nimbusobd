049a1ca392187368261fb9a04a90736bf86f8a72
payalpathak519 <payalpathak519@gmail.com>
2021-06-09 15:14:04 +0530

    echo

--- conn.php
+++ conn.php
@@ -1,8 +1,12 @@
-<?php 
-$conn = mysqli_connect('10.0.10.250', 'cron', '1234','asterisk');
-
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-echo "good";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "good"; ?>
+</body>
+</html>
